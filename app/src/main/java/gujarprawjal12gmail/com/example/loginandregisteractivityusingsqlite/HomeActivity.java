dc7a035a0d99b8e935fc8121fd7cf7bfa69384c5
gujarprajwal12 <gujarprajwal12@gmail.com>
2021-08-18 18:12:07 +0530

    Initial commit

---
+++ app/src/main/java/gujarprawjal12gmail/com/example/loginandregisteractivityusingsqlite/HomeActivity.java
@@ -0,0 +1,14 @@
+package gujarprawjal12gmail.com.example.loginandregisteractivityusingsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
